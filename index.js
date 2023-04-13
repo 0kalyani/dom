ffb765e52bbbb46788a404384fe53b44febc858f
bcakalyani1999 <bcakalyani1999@gmail.com>
2023-04-13 15:48:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var text=document.getElementById("a1");
+function myFunc(){
+    text.style.backgroundColor="yellow";
+    text.innerText="this is my text";
+}
+
+document.getElementById("a2").innerHTML="This is my page";
